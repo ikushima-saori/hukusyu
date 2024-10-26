f01ad3a151979d2d6bc1ca6d6561d497f6af1ed0
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-10-26 23:23:49 +0900

    復習

--- lesson.rb
+++ lesson.rb
@@ -1,2 +1,16 @@
-x = 50
-puts x - 10
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Testapp</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <%= csrf_meta_tags %>
+    <%= csp_meta_tag %>
+
+    <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+    <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
+  </head>
+
+  <body>
+    <%= yield %>
+  </body>
+</html>
