e9847366ec911aab046faccff18204ef6a40372e
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-10-27 21:02:22 +0900

    復習

--- lesson.rb
+++ lesson.rb
@@ -1,16 +1,69 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Testapp</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <%= csrf_meta_tags %>
-    <%= csp_meta_tag %>
+<%= stylesheet_link_tag 'public/customers' %>
+<div class="container">
 
-    <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
-    <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
-  </head>
+  <div class="row">
+    <h1>ユーザー一覧</h1>
+  </div>
 
-  <body>
-    <%= yield %>
-  </body>
-</html>
+  <div class="row">
+    <%= display_flash_messages %> <!--ヘルパーメソッドで指定-->
+  </div>
+
+  <div class="row">
+    <% @customers.each do |customer| %>
+    <table>
+        <tbody>
+            <tr>
+              <td>
+                <%= link_to customer_path(customer.id), class: 'link-button' do %>
+                  <div class="profile-container">
+                    <%= image_tag customer.get_profile_image(80, 80), class: 'profile-image' %>
+                    <div class="profile-name">
+                      <span><%= customer.name %></span>
+                    </div>
+                  </div>
+                <% end %>
+              </td>
+              <td>
+                <%= "アイデア数：#{customer.ideas.count}個 " %>
+              </td>
+              <% if current_customer != customer %>
+                <td>
+                  <div id="follow_btn_<%= customer.id %>">
+                    <%= render "public/relationships/followbtn", customer: customer %>
+                  </div>
+                </td>
+              <% end %>
+            </tr>
+            <tr>
+              <td colspan=3>好物</td>
+            </tr>
+            <tr>
+              <td colspan=3 class="preference-style"><%= content_tag(:div, customer.preference, class: "scroll") %></td>
+            </tr>
+            <tr>
+              <td colspan=3>苦手</td>
+            </tr>
+            <tr>
+              <td colspan=3 class="preference-style"><%= content_tag(:div, customer.weak, class: "scroll") %></td>
+            </tr>
+            <tr>  <!--follower_customerとfollowed_customerはcustomerモデルで定義-->
+              <td><%= link_to "フォロー：#{customer.follower_customer.count}人", follower_customer_path(customer), class: 'link-button' %></td>
+              <td><%= link_to "フォロワー：#{customer.followed_customer.count}人", followed_customer_path(customer), class: 'link-button' %></td>
+              <td>
+                <% if customer.favorites.count > 0 %>  <!---customerに紐づいたfavoritesテーブルのレコードがあれば-->
+                  <%= link_to "お気に入りアイデア：#{customer.favorites.count}個", favorite_customer_path(customer.id), class: 'link-button' %>
+                <% else %>
+                  お気に入り無し
+                <% end %>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      <% end %>
+  </div>
+
+  <div class="row">
+    <%= paginate @customers, class: "pagination" %>
+  </div>
+</div>
