1c1cbea830e2749b9170cc7f11b45f19feab5e4a
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-10-31 15:32:38 +0900

    復習

--- lesson.rb
+++ lesson.rb
@@ -1,41 +1,53 @@
-class ApplicationController < ActionController::Base
-  before_action :restrict_guest_access  #↓で定義  全てのcontrollerにゲストのアクセス制限を適用
-  helper_method :guest_user_signed_in?  #↓で定義  application.htmlでguest_user_signed_in?を使いたいのでhelper_methodとして設定(viewではcontrollerに定義したメソッドを直で使えない)
 
-  protected
+Rails.application.routes.draw do
 
-  #ゲストユーザーにアクセス制限をかける
-  def restrict_guest_access
-    if guest_user_signed_in?  #↓で定義
-      unless allowed_guest_actions?  #↓で定義
-        redirect_to root_path, alert: 'ゲストユーザーはTOPとaboutページのみ閲覧可能です。'
+  #登録系で使うデバイスのルーティングを先に書く
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {  #新規登録とパス変更は使わない
+    sessions: "admin/sessions"  #管理者が使うのはログインだけ
+  }
+  devise_for :customers,skip: [:passwords], controllers: {  #パス変更は使わない
+    registrations: "public/registrations",
+    sessions: 'public/sessions'  #ユーザーが使うのは新規登録とログイン
+  }
+
+  #ゲストログインのルーティング
+  devise_scope :customer do
+    post 'customer/guest_sign_in', to:'public/sessions#guest_sign_in'
+  end
+
+  #publicのルーティング
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about', to:'homes#about', as:'about'
+    get '/customers', to:'customers#index', as:'customers_index'  #「/customers」 = homes#index:customers_index_path
+    get 'customers/my_page', to:'customers#show', as:'my_page'  #「/customers/my_page」 = customers#show:my_page_path 自分のshow
+    resources :customers, only: [:show] do  #「/customers/:id」 = customers#show:customer_path 他人のshow
+      collection do
+        get 'my_page/edit', to:'customers#edit', as:'edit_my_page'  #「/customers/my_page/edit」 = customers#edit:edit_my_page_path
+        patch 'information', to:'customers#update', as:'update_information'  #「/customers/information」 = customers#update:update_information_path patchアクション
+        delete 'out'  #「/customers/out」 = customers#out outアクション
       end
+      member do
+        get 'favorite'  #「/customers/:id/favorite」 = customers#favorite
+        resource :relationships, only: [:create, :destroy]  #「/customers/:id/relationships」 = relationships#create&destroy:relationships_path
+          get "follower_customer" => "relationships#follower_customer", as:"follower"  #「/customers/:id/follower_customer」 = relationships#follower_customer:follower_customer_path
+          get "followed_customer" => "relationships#followed_customer", as:"followed"  #「/customers/:id/followed_customer」 = relationships#followed_customer:followed_customer_path
+      end  #↑resources :customers, only: [:show] doの中に設定されているので、as:follower_pathではなく_customerも含まれる
     end
-  end
-  def guest_user_signed_in? #メールアドレスがゲストのアドレスかどうか
-    customer_signed_in? && current_customer.email == Customer::GUEST_USER_EMAIL #GUEST_USER_EMAILはcustomerモデルで定義
-  end
-  def allowed_guest_actions?  #homesコントローラのtopとabout、sesstionsコントローラのdestroyしかアクセスできない
-    controller_name == 'homes' && %w[top about].include?(action_name) || controller_name == 'sessions' && action_name == 'destroy'
+    resources :ideas, only: %i[new create edit update destroy] do
+      resource :favorites, only: %i[create destroy]
+    end
+    resources :favorites, only: [] do
+      get 'favorite_customers', on: :member, to: 'favorites#favorite_customers', as: 'customers'
+    end
+    resources :tags, only: [:index, :show]
+    get "/search", to: "searches#search"
   end
 
-  #adminコントローラアクセス制限用
-   def admin_login!  #管理者としてログインしていなければアクセスさせない
-     unless admin_signed_in?
-       redirect_to root_path, alert: "管理者としてログインしてください。"
-     end
-   end
-  def customer_notview!  #ユーザーとしてログインしている場合はアクセスさせない
-    if customer_signed_in?
-      redirect_to root_path, alert: '管理者としてログインしてください。'
-    end
+  #adminのルーティング
+  get 'admin', to:'admin/homes#top'
+  namespace :admin do
+    resources :customers, only: [:edit, :update]
   end
-
-  #publicコントローラアクセス制限用
-   def admin_notview!  #管理者としてログインしている場合はアクセスさせない
-     if admin_signed_in?
-       redirect_to admin_path, alert: "ユーザーとしてログインしてください。"
-     end
-   end
 
 end
