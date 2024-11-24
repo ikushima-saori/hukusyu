cdde58dbf58627fc6bdf21d060a0b7567f7cad73
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-11-24 21:14:35 +0900

    復習

--- lesson.rb
+++ lesson.rb
@@ -3,42 +3,45 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8" />
-    <title>5章演習</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="caravan.css" />
+    <title>CARAVAN</title>
   </head>
+
   <body>
     <header>
-      <h1><a href="http://infratop.jp/">infratop</a></h1>
+      <h1 class="heading">
+        <a href="index.html">
+          CARAVAN
+        </a>
+      </h1>
     </header>
 
     <nav>
       <ul class="nav-list">
-        <li class="nav-list-item"><a href="http://web-camp.io/">webcamp</a></li>
         <li class="nav-list-item">
-          <a href="http://web-camp.io/pro/">webcamp pro</a>
+          <a href="index.html">Home</a>
         </li>
-        <li class="nav-list-item">
-          <a href="http://web-camp.io/woman/">webcamp woman</a>
-        </li>
-        <li class="nav-list-item">
-          <a href="http://web-camp.io/career/">webcamp career</a>
-        </li>
+        <li class="nav-list-item">About</li>
+        <li class="nav-list-item">Topic</li>
       </ul>
     </nav>
 
+    <div class="main-visual">
+      <div class="main-visual-content">
+        <p class="main-visual-text">
+          The world is a book<br />
+          and those who do not travel<br />
+          read only one page.
+        </p>
+        <p class="main-visual-name">
+          St. Augustine
+        </p>
+      </div>
+    </div>
+
     <div class="main">
-      <div class="box1"></div>
-      <div class="box2"></div>
-      <div class="box3"></div>
-      <div class="box4"></div>
-      <div class="box5"></div>
-      <div class="box6"></div>
-      <div class="box7"></div>
-      <div class="box8"></div>
-      <div class="box9"></div>
-      <div class="box10"></div>
-      <div class="box11"></div>
-      <div class="box12"></div>
+      <div class="blog"></div>
+      <div class="sidebar"></div>
     </div>
 
     <footer>
